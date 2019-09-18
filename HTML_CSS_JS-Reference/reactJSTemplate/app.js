cd6d4a220e84cd8ee8619fc941722c7eb45f1aba
Anya L Keller <kelleranyal@gmail.com>
2019-09-18 08:35:37 -0400

    began work on example react ap

--- HTML_CSS_JS-Reference/reactJSTemplate/app.js
+++ HTML_CSS_JS-Reference/reactJSTemplate/app.js
@@ -22,3 +22,29 @@ const server = http.createServer((req, res) => {
 server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
 });
+
+//react stuff
+("use strict");
+
+const e = React.createElement;
+
+class LikeButton extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { liked: false };
+    }
+
+    render() {
+        if (this.state.liked) {
+            return "You liked this.";
+        }
+
+        return e(
+            "button",
+            { onClick: () => this.setState({ liked: true }) },
+            "Like"
+        );
+    }
+}
+const domContainer = $("#reactSandbox");
+ReactDOM.render(e(LikeButton), domContainer);
