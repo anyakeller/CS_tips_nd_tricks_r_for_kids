2fd8dffa71f41038ce527371fcd71406800311cf
Anya L Keller <kelleranyal@gmail.com>
2019-09-19 19:18:53 -0400

    haha Ron, I got it working without your help

--- HTML_CSS_JS-Reference/example-react-app/src/App.js
+++ HTML_CSS_JS-Reference/example-react-app/src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
 import "./App.css";
+import Clock from "./Clock.js";
 
 function indexContents() {
+    console.log(Clock);
     return (
         <div id="index-contents">
             <h1>I am your index page!!!</h1>
             <FartThePotato potatoType="Japanese Sweet Potato" />
             <FartThePotato potatoType="Garnet" />
             <FartThePotato potatoType="Ruby" />
+            <Clock />
         </div>
     );
 }
---
+++ HTML_CSS_JS-Reference/example-react-app/src/Clock.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+// react JS website  example https://reactjs.org/docs/state-and-lifecycle.html
+class Clockelem extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { date: new Date() };
+    }
+
+    componentDidMount() {
+        this.timerID = setInterval(() => this.tick(), 1000);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+    }
+
+    tick() {
+        this.setState({
+            date: new Date()
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Hello, world!</h1>
+                <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+            </div>
+        );
+    }
+}
+
+// what I added
+export default Clockelem;
